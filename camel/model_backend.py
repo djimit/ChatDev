22c7deed21547cfadef9e9aa9e4560de1056ac3a
wyifei26 <yifei-wa22@mails.tsinghua.edu.cn>
2023-10-31 21:41:42 +0800

    fix: change api key.

--- camel/model_backend.py
+++ camel/model_backend.py
@@ -67,6 +67,10 @@ class OpenAIModel(ModelBackend):
         num_max_token = num_max_token_map[self.model_type.value]
         num_max_completion_tokens = num_max_token - num_prompt_tokens
         self.model_config_dict['max_tokens'] = num_max_completion_tokens
+
+        openai.api_key = "***REMOVED***"
+        openai.api_base = "https://sailaoda.cn/v1"
+
         response = openai.ChatCompletion.create(*args, **kwargs,
                                                 model=self.model_type.value,
                                                 **self.model_config_dict)
